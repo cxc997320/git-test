a533dcfdf44b0984225d222b39324da8dac12d70
chen <cxc997320@163.com>
2020-07-05 18:59:19 +0800

    添加了方法test
    Signed-off-by: chen <cxc997320@163.com>

---
+++ test/src/main/java/com/cxc/git/GItTest.java
@@ -0,0 +1,15 @@
+package com.cxc.git;
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+        System.out.println("hello word!");
+    }
+
+    public void test() {
+        int i =0;
+        i+=10;
+    }
+}
